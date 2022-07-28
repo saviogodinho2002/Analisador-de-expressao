7b12fe9731f7554bd4725e371196ce424256d1b9
saviogodinho <saviogodinho2002@gmai.com>
2022-07-28 13:44:25 -0300

    ajustes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,85 +1,56 @@
-object regexExp {
+object RegexExp {
     val mulDivRegex = "(\\d+|\\d+\\.\\d+)[*/]{1}(\\d+\\.\\d+|\\d+)".toRegex();
     val addSubRegex = "(\\d+|\\d+\\.\\d+)[+-]{1}(\\d+\\.\\d+|\\d+)".toRegex();
     val openParenthesesRegex = "\\(".toRegex();
     val closeParenthesesRegex = "\\)".toRegex();
     val finalResult = "^((\\d+\\.\\d+)|(\\d+))$".toRegex();
     val numberRegex = "((\\d+\\.\\d+)|(\\d+))".toRegex();
-    val addSubSymbols = "[+-]{1}".toRegex();
-    val mulDivSimbols = "[*/]{1}".toRegex();
-
-
+    val genericExpression = "((\\d+\\.\\d+)|(\\d+))[-+*/]((\\d+\\.\\d+)|(\\d+))".toRegex()
+    val operationSimbols = "[-+*/]".toRegex();
+    
 }
 
-fun calculateMulDiv(expression: String): String {
-    val match = regexExp.mulDivRegex.findAll(expression);
+fun calculate(expression: String): String {
+    val match = RegexExp.genericExpression.findAll(expression);
 
     val eqExpression = match.first().value;
 
-    val matchNumber = regexExp.numberRegex.findAll(eqExpression)
+    val matchNumber = RegexExp.numberRegex.findAll(eqExpression)
 
     val firstNumber = matchNumber.first().value;
     val secondNumber = matchNumber.elementAt(1).value
 
-    var result = when (regexExp.mulDivSimbols.findAll(eqExpression).first().value) {
+    var result = when (RegexExp.operationSimbols.findAll(eqExpression).first().value) {
         "*" -> (firstNumber.toDouble() * secondNumber.toDouble()).toString()
         "/" -> (firstNumber.toDouble() / secondNumber.toDouble()).toString()
+        "+" -> (firstNumber.toDouble() + secondNumber.toDouble()).toString()
+        "-" -> (firstNumber.toDouble() - secondNumber.toDouble()).toString()
         else -> "0";
     }
     return searchExpressions(expression.replace(eqExpression, result));
 
 }
 
-fun calculateAddSub(expression: String): String {
-    val match = regexExp.addSubRegex.findAll(expression);
-
-    val eqExpression = match.first().value;
-
-    val matchNumber = regexExp.numberRegex.findAll(eqExpression)
-
-    println("$eqExpression")
-
-    val firstNumber = matchNumber.first().value;
-    val secondNumber = matchNumber.elementAt(1).value
-
-    var result = when (regexExp.addSubSymbols.findAll(eqExpression).first().value) {
-        "+" -> (firstNumber.toDouble() + secondNumber.toDouble()).toString()
-        "-" -> (firstNumber.toDouble() - secondNumber.toDouble()).toString()
-        else -> "Expressão mal formulada";
-    }
-
-
-    return searchExpressions(expression.replace(eqExpression, result));
-}
-
-
 fun searchParentheses(expression: String): String {
 
 
-    val matchOpen = regexExp.openParenthesesRegex.findAll(expression);
-    val matchClose = regexExp.closeParenthesesRegex.findAll(expression);
+    val matchOpen = RegexExp.openParenthesesRegex.findAll(expression);
+    val matchClose = RegexExp.closeParenthesesRegex.findAll(expression);
     val open = matchOpen.first().range.first;
     try {
-        var nextClose:Int;// = matchClose.elementAt(0).range.first;
-
-       // println("$open $nextClose")
-
-        //enquanto olhando a string o  euando os count de nextClose e close forem diferentes
-        var expBetween:String;// = expression.subSequence(open + 1, nextClose).toString();
+        var nextClose:Int;
+        var expBetween:String;
 
         var openCount:Int;
 
         var iterator:Int = 0;
         do{
-            nextClose = matchClose.elementAt(iterator).range.first; //pegar o indice do proximo fechamento no iterator
-            expBetween = expression.subSequence(open + 1, nextClose).toString(); // substring do '( 'ate o primeiro ')'
+            nextClose = matchClose.elementAt(iterator).range.first;
+            expBetween = expression.subSequence(open + 1, nextClose).toString();
+            openCount = RegexExp.openParenthesesRegex.findAll(expBetween).count();
+            expBetween = expression.subSequence(open + 1, matchClose.elementAt( openCount ).range.first   ).toString()
 
-            openCount = regexExp.openParenthesesRegex.findAll(expBetween).count(); //confere quantas aberturas tem no intervalo
-
-            expBetween = expression.subSequence(open + 1, matchClose.elementAt( openCount ).range.first   ).toString() // pega o intervalo ate a abertura atual
-
-
-        }while ( openCount != iterator++); // o numero de abertura deve ser igual ao do contador
+        }while ( openCount != iterator++);
 
         val nExp = expression.replace("($expBetween)", searchExpressions(expBetween));
 
@@ -94,16 +65,16 @@ fun searchParentheses(expression: String): String {
 
 fun searchExpressions(expression: String): String {
 
-    if(regexExp.finalResult.containsMatchIn(expression))
+    if(RegexExp.finalResult.containsMatchIn(expression))
         return expression;
-    else if (regexExp.openParenthesesRegex.containsMatchIn(expression) || regexExp.closeParenthesesRegex.containsMatchIn(expression))
+    else if (RegexExp.openParenthesesRegex.containsMatchIn(expression) || RegexExp.closeParenthesesRegex.containsMatchIn(expression))
         return searchParentheses(expression);
 
-    else if (regexExp.mulDivRegex.containsMatchIn(expression))
-        return calculateMulDiv(expression);
+    else if (RegexExp.mulDivRegex.containsMatchIn(expression))
+        return calculate(expression);
 
-    else if (regexExp.addSubRegex.containsMatchIn(expression))
-        return calculateAddSub(expression);
+    else if (RegexExp.addSubRegex.containsMatchIn(expression))
+        return calculate(expression);
 
     return "0";
 }
@@ -112,7 +83,7 @@ fun searchExpressions(expression: String): String {
 fun main() {
 
 
-    val expression: String = "((10+62)/((4)+(5)))".replace(" ","");
+    val expression: String = "((10+62)/(2+2))".replace(" ","");
 
     println(searchExpressions(expression));
 
